569488debf16c3c527009fae47554c0b75f3069f
Evan Lloyd <eulloyd@gmail.com>
2014-08-01 21:05:47 +0900

    Add test for directive

--- optin/appSpec.js
+++ optin/appSpec.js
@@ -2,19 +2,20 @@ describe('optin', function(){
   beforeEach(module('myApp'));
   beforeEach(module('optin.html'));
   beforeEach(inject(function($rootScope, $compile){
-    var brand = 'brand-logo'
+    var brand = 'brand-logo';
     var html="";
     html += "<opt-in>";
     html += "<div class='"+brand+"'></div>";
     html += "</opt-in>";
 
     scope = $rootScope.$new();
-    compiled = $compile(html)
+    compiled = $compile(html);
     element = compiled(scope);
     scope.$digest();
   }));
 
   it('should render the element correctly', function(){
-    expect(element.find('.left').text()).toContain('div');
+    console.log(element);
+    expect(element.find('.brand-logo').length).toBe(1);
   })
 })
